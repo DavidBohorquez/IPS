0058c64d163a182997171691488d3214849503b6
DavidBohorquez <blueplayer-10@hotmail.com>
2019-07-21 23:41:29 -0500

    añadida multa

---
+++ IPS/src/logica/Multa.java
@@ -0,0 +1,37 @@
+package logica;
+
+/**
+ *
+ * @author David Bohorquez
+ */
+public class Multa {
+    private int id;
+    private char estado;
+    private int valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+    
+}
